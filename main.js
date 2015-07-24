23ad53f2999246b1bc0d2d5cc28b849154a59292
apple <apple@appledeiMac.local>
2015-07-25 00:20:02 +0800

    拉拉

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'gameDiv');
 game.global = {
     score: 0,
-    showDebug:false
+    showDebug:false,
+    commb:0
 };
+
 
 // We create our only state
 var playState = {
@@ -15,23 +17,37 @@ var playState = {
 
     	game.load.spritesheet('musicStage', '/game-assets/music_stage_all.png',1024,179);
     	game.load.spritesheet('hiteArea', '/game-assets/hite_area_all.png',123,123);
-
     	game.load.image('hiteAreaMask', '/game-assets/area_click_mask.png');
-
-        game.load.audio('beat', ['game-assets/taiko-normal-hitnormal.mp3', 'assets/taiko-normal-hitnormal.ogg']);
+    	game.load.image('flower', '/game-assets/flower.png');
+        game.load.audio('beat', ['game-assets/normal-hitnormal.mp3', 'assets/normal-hitnormal.ogg']);
 
 
     },
 	create: function() { 
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
+		//節拍的佈景
 		this.sp_musicStage = game.add.sprite(0, 226, 'musicStage');
 		this.sp_musicStage.anchor.setTo(0, 0);
 		this.sp_musicStage.animations.add('musicStageBeat', [1,0], 30);
-		
+		//打擊區
 		this.sp_hiteArea = game.add.sprite(167, 236, 'hiteArea');
 		this.sp_hiteArea.anchor.setTo(0, 0);
 		this.sp_hiteArea.animations.add('hiteAreaBeat', [1,0], 30);
+		//打擊區煙火
+		this.sp_flower = game.add.image(228, 298, 'flower');
+		this.sp_flower.anchor.setTo(0.5, 0.5);
+		this.sp_flower.alpha = 0.5;
+		this.sp_flower.scale = {x:0,y:0};
+
+		//連擊數
+		this.combLabel = game.add.text(10, 318, '000', {
+            font: '73px PassionOne',
+            fill: '#000000'
+        });
+
+        this.combLabel.anchor.setTo(0, 0.5);
+
 
 
 		//this.sp_musicStage.setSize(123, 123, 0, 0);
@@ -52,6 +68,7 @@ var playState = {
 
         //event
         this.cursor.left.onDown.add(this.beat, this);
+        this.cursor.left.onDown.add(this.hit, this);
 
 		
 
@@ -70,6 +87,18 @@ var playState = {
 		this.sp_hiteArea.animations.play('hiteAreaBeat');
 		this.sound_beat.play();
 
+	},
+	hit: function(){
+
+		game.add.tween(this.sp_flower.scale)
+		.to({x: 1,y:1}, 100, "Linear", false, 0 , 0 , false)
+		.to({x: 0,y:0}, 100, "Linear", false, 0 , 0 , false)
+		.start();
+
+		game.add.tween(this.sp_flower)
+		.to({angle: 90}, 100, "Linear", false, 0 , 0 , false)
+		.to({angle: 0}, 100, "Linear", false, 0 , 0 , false)
+		.start();
 	}
 };
 
---
+++ mian_exp.js
@@ -0,0 +1,348 @@
+// We initialising Phaser
+// Initialise Phaser
+var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+game.global = {
+    score: 0
+};
+
+
+var bootState = {
+    preload: function () {
+        // Load the image
+        game.load.image('progressBar', 'assets/progressBar.png');
+    },
+    create: function () {
+        // Set some game settings
+        game.stage.backgroundColor = '#3498db';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        // If the device is not a desktop, so it's a mobile device
+        // If the device is not a desktop, so it's a mobile device
+        if (!game.device.desktop) {
+            // Set the type of scaling to 'show all'
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            // Add a blue color to the page, to hide the white borders we might have
+            document.body.style.backgroundColor = '#3498db';
+            // Set the min and max width/height of the game
+            game.scale.minWidth = 250;
+            game.scale.minHeight = 170;
+            game.scale.maxWidth = 1000;
+            game.scale.maxHeight = 680;
+
+            // Center the game on the screen
+            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignVertically = true;
+
+            // Apply the scale changes
+            game.scale.setScreenSize(true);
+        }
+
+        // Start the load state
+        game.state.start('load');
+    }
+};
+
+
+var loadState = {
+
+    preload: function () {
+        var loadingLabel = game.add.text(game.world.centerX, 150, 'loading...', {
+            font: '30px Arial',
+            fill: '#ffffff'
+        });
+        loadingLabel.anchor.setTo(0.5, 0.5);
+
+        var progressBar = game.add.sprite(game.world.centerX, 200, 'progressBar');
+        progressBar.anchor.setTo(0.5, 0.5);
+        game.load.setPreloadSprite(progressBar);
+
+        game.load.spritesheet('player', 'assets/player2.png', 20, 20);
+        game.load.image('enemy', 'assets/enemy.png');
+        game.load.image('coin', 'assets/coin.png');
+        game.load.image('pixel', 'assets/pixel.png');
+        game.load.image('background', 'assets/background.png');
+        game.load.spritesheet('mute', 'assets/muteButton.png', 28, 22);
+        game.load.image('jumpButton', 'assets/jumpButton.png');
+        game.load.image('rightButton', 'assets/rightButton.png');
+        game.load.image('leftButton', 'assets/leftButton.png');
+
+        game.load.audio('jump', ['assets/jump.ogg', 'assets/jump.mp3']);
+        game.load.audio('coin', ['assets/coin.ogg', 'assets/coin.mp3']);
+        game.load.audio('dead', ['assets/dead.ogg', 'assets/dead.mp3']);
+
+        this.load.image('tileset', 'assets/tileset.png');
+        this.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
+    },
+    create: function () {
+        game.state.start('menu');
+         
+    }
+};
+
+var menuState = {
+
+    create: function () {
+        game.add.image(0, 0, 'background');
+
+        var nameLabel = game.add.text(game.world.centerX, -50, 'Super Coin Box', {
+            font: '70px Geo',
+            fill: '#ffffff'
+        });
+        nameLabel.anchor.setTo(0.5, 0.5);
+        game.add.tween(nameLabel).to({
+            y: 80
+        }, 1000).easing(Phaser.Easing.Bounce.Out).start();
+/*
+        if (!localStorage.getItem('bestScore')) {
+            localStorage.setItem('bestScore', 0);
+        }
+
+        if (game.global.score > localStorage.getItem('bestScore')) {
+            localStorage.setItem('bestScore', game.global.score);
+        }
+
+        var text = 'score: ' + game.global.score + '\nbest score: ' + localStorage.getItem('bestScore');*/
+        var text = '';
+        var scoreLabel = game.add.text(game.world.centerX, game.world.centerY, text, {
+            font: '25px Arial',
+            fill: '#ffffff',
+            align: 'center'
+        });
+        scoreLabel.anchor.setTo(0.5, 0.5);
+
+        if (this.game.device.desktop) {
+            var text = 'press the up arrow key to start';
+        } else {
+            var text = 'touch the screen to start';
+        }
+        var startLabel = game.add.text(game.world.centerX, game.world.height - 80, text, {
+            font: '25px Arial',
+            fill: '#ffffff'
+        });
+        startLabel.anchor.setTo(0.5, 0.5);
+
+        game.add.tween(startLabel).to({
+            angle: -2
+        }, 500).to({
+            angle: 2
+        }, 500).loop().start();
+
+        this.muteButton = game.add.button(20, 20, 'mute', this.toggleSound, this);
+        this.muteButton.input.useHandCursor = true;
+        if (game.sound.mute) {
+            this.muteButton.frame = 1;
+        }
+
+        var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        upKey.onDown.addOnce(this.start, this);
+
+        game.input.onDown.addOnce(this.start, this);
+    },
+    toggleSound: function () {
+        game.sound.mute = !game.sound.mute;
+        this.muteButton.frame = game.sound.mute ? 1 : 0;
+    },
+    start: function () {
+        game.state.start('play');
+    }
+};
+
+// We create our only state
+var playState = {
+
+	create: function() { 
+		this.cursor = game.input.keyboard.createCursorKeys();
+		game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT]);
+		this.wasd = {
+			up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+			left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+			right: game.input.keyboard.addKey(Phaser.Keyboard.D)
+		};
+
+		game.global.score = 0;
+		this.createWorld();
+		if (!game.device.desktop) {
+			this.addMobileInputs();
+		}
+
+		this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+		game.physics.arcade.enable(this.player); 
+		this.player.anchor.setTo(0.5, 0.5);
+		this.player.body.gravity.y = 500;
+		this.player.animations.add('right', [1, 2], 8);
+		this.player.animations.add('left', [3, 4], 8);
+
+		this.enemies = game.add.group();
+		this.enemies.enableBody = true;
+		this.enemies.createMultiple(10, 'enemy');
+
+		this.coin = game.add.sprite(60, 140, 'coin');
+		game.physics.arcade.enable(this.coin); 
+		this.coin.anchor.setTo(0.5, 0.5);
+
+		this.scoreLabel = game.add.text(30, 30, 'score: 0', { font: '18px Arial', fill: '#ffffff' });	
+
+		this.emitter = game.add.emitter(0, 0, 15);
+		this.emitter.makeParticles('pixel');
+		this.emitter.setYSpeed(-150, 150);
+		this.emitter.setXSpeed(-150, 150);
+		this.emitter.gravity = 0;
+
+		this.jumpSound = game.add.audio('jump');
+		this.coinSound = game.add.audio('coin');
+		this.deadSound = game.add.audio('dead');	
+		
+		this.nextEnemy = 0;
+	},
+
+	update: function() {
+		game.physics.arcade.overlap(this.player, this.enemies, this.playerDie, null, this);
+		game.physics.arcade.overlap(this.player, this.coin, this.takeCoin, null, this);
+		game.physics.arcade.collide(this.player, this.layer);
+		game.physics.arcade.collide(this.enemies, this.layer);
+
+		if (!this.player.inWorld) {
+			this.playerDie();
+		}
+
+		this.movePlayer();
+
+		if (this.nextEnemy < game.time.now) {
+			var start = 4000, end = 1000, score = 100;
+			var delay = Math.max(start - (start-end)*game.global.score/score, end);
+			    
+			this.addEnemy();
+			this.nextEnemy = game.time.now + delay;
+		}
+	},
+
+	movePlayer: function() {
+		if (this.cursor.left.isDown || this.wasd.left.isDown || this.moveLeft) {
+			this.player.body.velocity.x = -200;
+			this.player.animations.play('left');
+		}
+		else if (this.cursor.right.isDown || this.wasd.right.isDown || this.moveRight) {
+			this.player.body.velocity.x = 200;
+			this.player.animations.play('right');
+		}
+		else {
+			this.player.body.velocity.x = 0;
+ 			this.player.animations.stop(); 
+	        this.player.frame = 0; 
+		}
+		
+		if (this.cursor.up.isDown || this.wasd.up.isDown) {
+			this.jumpPlayer();
+		}
+	},
+
+	jumpPlayer: function() {
+		if (this.player.body.onFloor()) {
+			this.jumpSound.play();
+			this.player.body.velocity.y = -320;	
+		}		
+	},
+
+	addEnemy: function() {
+		var enemy = this.enemies.getFirstDead();
+		if (!enemy) {
+			return;
+		}
+
+		enemy.anchor.setTo(0.5, 1);
+		enemy.reset(game.world.centerX, 0);
+		enemy.body.gravity.y = 500;
+		enemy.body.bounce.x = 1;
+		enemy.body.velocity.x = 100 * Phaser.Math.randomSign();
+
+		enemy.checkWorldBounds = true;
+		enemy.outOfBoundsKill = true;
+	},
+
+	takeCoin: function(player, coin) {
+		game.global.score += 5;
+		this.scoreLabel.text = 'score: ' + game.global.score;
+
+		this.updateCoinPosition();
+
+		this.coinSound.play();
+		game.add.tween(this.player.scale).to({x:1.3, y:1.3}, 50).to({x:1, y:1}, 150).start();
+		this.coin.scale.setTo(0, 0);
+		game.add.tween(this.coin.scale).to({x: 1, y:1}, 300).start();
+	},
+
+	updateCoinPosition: function() {
+		var coinPosition = [
+			{x: 140, y: 60}, {x: 360, y: 60}, 
+			{x: 60, y: 140}, {x: 440, y: 140}, 
+			{x: 130, y: 300}, {x: 370, y: 300} 
+		];
+
+		for (var i = 0; i < coinPosition.length; i++) {
+			if (coinPosition[i].x === this.coin.x) {
+				coinPosition.splice(i, 1);
+			}
+		}
+
+		var newPosition = coinPosition[game.rnd.integerInRange(0, coinPosition.length-1)];
+		this.coin.reset(newPosition.x, newPosition.y);
+	},
+
+	playerDie: function() {
+		if (!this.player.alive) {
+			return;
+		}
+		
+		this.player.kill();
+
+		this.deadSound.play();
+		this.emitter.x = this.player.x;
+		this.emitter.y = this.player.y;
+		this.emitter.start(true, 600, null, 15);
+
+		game.time.events.add(1000, this.startMenu, this);
+	},
+
+	startMenu: function() {
+		game.state.start('menu');
+	},
+
+	createWorld: function() {
+ 		this.map = game.add.tilemap('map'); 
+        this.map.addTilesetImage('tileset'); 
+        this.layer = this.map.createLayer('Tile Layer 1');
+        this.layer.resizeWorld(); 
+        this.map.setCollision(1);
+	},
+
+	addMobileInputs: function() {
+		this.jumpButton = game.add.sprite(350, 247, 'jumpButton');
+		this.jumpButton.inputEnabled = true;
+		this.jumpButton.events.onInputDown.add(this.jumpPlayer, this);
+		this.jumpButton.alpha = 0.5;
+
+		this.moveLeft = false;
+		this.moveRight = false;
+
+		this.leftButton = game.add.sprite(50, 247, 'leftButton');
+		this.leftButton.inputEnabled = true;
+		this.leftButton.events.onInputOver.add(function(){this.moveLeft=true;}, this);
+		this.leftButton.events.onInputOut.add(function(){this.moveLeft=false;}, this);
+		this.leftButton.events.onInputDown.add(function(){this.moveLeft=true;}, this);
+		this.leftButton.events.onInputUp.add(function(){this.moveLeft=false;}, this);
+		this.leftButton.alpha = 0.5;
+
+		this.rightButton = game.add.sprite(130, 247, 'rightButton');
+		this.rightButton.inputEnabled = true;
+		this.rightButton.events.onInputOver.add(function(){this.moveRight=true;}, this);
+		this.rightButton.events.onInputOut.add(function(){this.moveRight=false;}, this);
+		this.rightButton.events.onInputDown.add(function(){this.moveRight=true;}, this);
+		this.rightButton.events.onInputUp.add(function(){this.moveRight=false;}, this);
+		this.rightButton.alpha = 0.5;
+	}
+};
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('play', playState);
+game.state.start('boot');
